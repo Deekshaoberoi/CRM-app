d574398b2ea30f5717664b0ebcfab8001423af3b
Deekshaaoberoi <DEEKSHAOBEROI90@GMAIL.COM>
2025-01-11 15:52:42 +0530

    CRM - Project

---
+++ app/Models/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable , withData;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'name' => 'string',
+            'email' => 'string',
+            'password' => 'string',
+            'phone' => 'string',
+        ];
+    }
+}
