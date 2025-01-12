0336dc5013aa14215c29e5ab3887801f54ae4876
Deekshaaoberoi <DEEKSHAOBEROI90@GMAIL.COM>
2025-01-12 11:49:20 +0530

    Controller

---
+++ app/Http/Controllers/EmailCampaignController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\EmailCampaignRepository;
+use Illuminate\Support\Facades\Redirect;
+use App\Data\EmailCampaignData;
+
+class EmailCampaignController extends Controller
+{
+    public function __construct(private EmailCampaignRepository $emailCampaignRepository) {}
+
+    public function index()
+    {
+        return view('Email-campaigns.index');
+    }
+
+    public function create()
+    {
+        return view('Email-campaigns.create');
+    }
+
+    public function store(EmailCampaignData $request)
+    {
+        $this->emailCampaignRepository->storeEmailCampaign($request->validated());
+
+        return Redirect::route('Email-campaigns.index');
+    }
+
+    public function update(EmailCampaignData $request, int $id)
+    {
+        $this->emailCampaignRepository->updateEmailCampaign($request->validated(), $this->emailCampaignRepository->getEmailCampaignById($id));
+
+        return Redirect::route('Email-campaigns.index');
+    }
+}
